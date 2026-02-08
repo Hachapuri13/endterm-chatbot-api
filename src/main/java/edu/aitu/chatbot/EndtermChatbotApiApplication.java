fdbce236f12b97c5e54a964979173f0b0e223a74
Hachapuri13 <k.potato4ver@gmail.com>
2026-02-08 16:19:25 +0500

    chore: init spring boot project

---
+++ src/main/java/edu/aitu/chatbot/EndtermChatbotApiApplication.java
@@ -0,0 +1,13 @@
+package edu.aitu.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndtermChatbotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndtermChatbotApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/aitu/chatbot/EndtermChatbotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.aitu.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndtermChatbotApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
